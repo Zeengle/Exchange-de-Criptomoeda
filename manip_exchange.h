aee4e2242d06d541eaceb920e3929bc4a6b88324
Gabriel Engle <gab.engle.dev@gmail.com>
2024-09-13 14:18:41 -0300

    Update manip_exchange.h

---
+++ manip_exchange.h
@@ -0,0 +1,24 @@
+#ifndef manip_exchange
+#define manip_exchange
+
+#include <stdio.h>
+#include <string.h>
+<<<<<<< Updated upstream
+#include <time.h>
+#include <stdlib.h>
+=======
+
+>>>>>>> Stashed changes
+
+void menu(int usuariologado);
+void menuinicial(int usuariologado);
+void consultarsaldo(int usuariologado);
+void consultarextrato(int usuariologado);
+void depositar(int usuariologado);
+void sacar(int usuariologado);
+void comprar(int usuariologado);
+void vender(int usuariologado);
+void atualizar();
+void verificação(int usuariologado);
+
+#endif

2aa343ed3dbb3ceb3c9f1080418c61a822e0f0aa
nThzzzz <161968206+nThzzzz@users.noreply.github.com>
2024-09-25 20:58:29 -0300

    Salvar em .bin

--- main.c
+++ main.c
@@ -6,12 +6,23 @@
 #include "manip_exchange.h"
 #include <math.h>
 
+/*---------------------------------------MAIN----------------------------------------*/
+
 int main() {
+    FILE *file;
     pessoa pessoas[10];
     float cotacaoBTC = 100;
     float cotacaoETH = 50;
     float cotacaoXRP = 25;
     int usuariologado = -1;
+
+    int verificacao = arquivoexiste("dados.bin");
+
+    if(verificacao == 0){ // O arquivo existe
+        escrever(pessoas, 10, cotacaoBTC, cotacaoETH, cotacaoXRP);
+    }else{
+        ler(&file, pessoas, 10, &cotacaoBTC, &cotacaoETH, &cotacaoXRP);
+    }
     menuinicial(pessoas, cotacaoBTC, cotacaoETH, cotacaoXRP, usuariologado);
     return 0;
 }
